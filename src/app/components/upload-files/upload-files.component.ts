9db506390b108a4f6cf5b7fbb5fd6bf3fe102ce7
slhmohamed <salahmohamed0944@gmail.com>
2020-12-16 20:42:32 +0100

    first commit

---
+++ src/app/components/upload-files/upload-files.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { UploadFileService } from 'src/app/services/upload-file.service';
+import { HttpEventType, HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-upload-files',
+  templateUrl: './upload-files.component.html',
+  styleUrls: ['./upload-files.component.scss']
+})
+export class UploadFilesComponent implements OnInit {
+
+ selectedFiles:any;
+  currentFile:any;
+  progress = 0;
+  message = '';
+
+  fileInfos: any;
+
+  constructor(private uploadService: UploadFileService) { }
+
+  ngOnInit(): void {
+    this.fileInfos = this.uploadService.getFiles();
+  }
+
+  selectFile(event:any): void {
+    this.selectedFiles = event.target.files;
+  }
+
+  upload(): void {
+    this.progress = 0;
+
+    this.currentFile = this.selectedFiles.item(0);
+    this.uploadService.upload(this.currentFile).subscribe(
+      event => {
+        if (event.type === HttpEventType.UploadProgress) {
+       
+          this.progress = 100;
+        } else if (event instanceof HttpResponse) {
+          this.message = event.body.message;
+          this.fileInfos = this.uploadService.getFiles();
+        }
+      },
+      err => {
+        this.progress = 0;
+        this.message = 'Could not upload the file!';
+        this.currentFile = undefined;
+      });
+
+    this.selectedFiles = undefined;
+  }
+}
